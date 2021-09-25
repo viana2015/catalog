759738cfbb7e31ede8db817bc391579e2c875742
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-09-24 22:28:24 -0300

    DTO

--- backend/src/main/java/com/jrcg/catalog/services/CategoryService.java
+++ backend/src/main/java/com/jrcg/catalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.jrcg.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jrcg.catalog.dto.CategoryDTO;
 import com.jrcg.catalog.entities.Category;
 import com.jrcg.catalog.repositories.CategoryRepository;
 
@@ -15,7 +17,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	//@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category>list = repository.findAll();
+		List<CategoryDTO>listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDTO;
 	}
 }
